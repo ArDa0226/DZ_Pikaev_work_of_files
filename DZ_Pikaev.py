c4c2782da97c24fabfd04d558ff3a1f3cbf91f1e
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-04-22 19:29:59 +0500

    Update DZ_Pikaev.py
    Добавил код.

--- DZ_Pikaev.py
+++ DZ_Pikaev.py
@@ -1,3 +1,17 @@
 
-file_1 = 'webinartest.txt'
-with open()
+with open('webinartest.txt', mode='r', encoding='utf8') as op_fl:
+    numbers = []
+    num = ''
+    for elem in op_fl:
+        for char in elem:
+            if char.isdigit():
+                num += char
+
+            else:
+                if len(num) != 0:
+                    numbers.append(int(num))
+                    num = ''
+
+
+with open('new_list.txt', mode='w', encoding='utf8') as new_file:
+    new_file.write(str(numbers))
